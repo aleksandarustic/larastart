58cb437629465e3a994fb13a6ed7cf383c76ac8f
aleksandar <aleksandarustic2@gmail.com>
2018-09-18 08:52:49 +0200

    3

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('type')->default('user');
+            $table->mediumText('bio')->nullable();
+            $table->string('photo')->default('profile.png');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,12 +9,39 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import { Form, HasError, AlertError } from 'vform'
 import VueRouter from 'vue-router'
+import moment from 'moment'
+import VueProgressBar from 'vue-progressbar'
+import swal from 'sweetalert2'
+
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+
+window.Form = Form;
+window.swal = swal;
+window.toast = toast;
+
 Vue.use(VueRouter);
+
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+
+
+Vue.use(VueProgressBar,{
+    color:'rgb(143,255,199)',
+    failedColor:'red',
+    height:'3px'
+});
 
 let routes = [
     {path:'/dashboard', component:require('./components/Dashboard.vue')},
-    {path:'/profile', component:require('./components/Profile.vue')}
+    {path:'/profile', component:require('./components/Profile.vue')},
+    {path:'/users', component:require('./components/Users.vue')},
 ];
 
 
@@ -22,6 +49,18 @@ const router = new VueRouter({
     mode:'history',
     routes // short for `routes: routes`
 });
+
+
+
+
+Vue.filter('upText',function (text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+Vue.filter('myDate',function (created) {
+    return moment(created).format('MMMM Do YYYY');
+});
+
+window.Fire = new Vue();
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
